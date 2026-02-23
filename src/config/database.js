77ecaf75a77954f498eb4ab5ea84ff959803f84d
Aaquib Rais <aaquibrais12345@gmail.com>
2026-02-23 23:40:25 +0530

    connection

--- src/config/database.js
+++ src/config/database.js
@@ -3,16 +3,16 @@ const { Sequelize } = require('sequelize');
 
 console.log(process.env.MYSQL_PUBLIC_URL);
 
-console.log(process.env.MYSQLDATABASE);
+console.log(process.env.MYSQL_DATABASE);
 console.log(process.env.MYSQLUSER);
 console.log(process.env.MYSQLPASSWORD);
 console.log(process.env.MYSQLHOST);
 console.log(process.env.MYSQLPORT);
 
 const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE || 'gossip_db',
+  process.env.MYSQL_DATABASE || 'railway',
   process.env.MYSQLUSER || 'root',
-  process.env.MYSQLPASSWORD || '',
+  process.env.MYSQLPASSWORD || 'pDUPnKPIAyvjKSWkyGFeYyDxjRQCcvUW',
   {
     host: process.env.MYSQLHOST || 'mysql.railway.internal',
     port: parseInt(process.env.MYSQLPORT, 10) || 3306,

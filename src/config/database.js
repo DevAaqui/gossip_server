b3215d88335a9a633cb4f374e2135a1881cfcfef
Aaquib Rais <aaquibrais12345@gmail.com>
2026-02-23 23:41:57 +0530

    some changes

--- src/config/database.js
+++ src/config/database.js
@@ -23,7 +23,7 @@ const sequelize = new Sequelize(
       acquire: 30000,
       idle: 10000,
     },
-    logging: true //process.env.NODE_ENV === 'development' ? console.log : true,
+    logging: process.env.NODE_ENV === 'development' ? console.log : false,
   }
 );
 
